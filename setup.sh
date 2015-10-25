eeeaf1212e05769b9b0659f8d4ad3123e8a76451
Ubuntu <ubuntu@ip-172-31-20-222.ap-northeast-1.compute.internal>
2015-10-25 13:34:17 +0000

    fix permission issue

--- setup.sh
+++ setup.sh
@@ -9,11 +9,11 @@
 # install pip
 wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
 
-python get-pip.py
+sudo python get-pip.py
 
 rm get-pip.py
 
 # install virtualenv
-pip install virtualenv
+sudo pip install virtualenv
 
 
