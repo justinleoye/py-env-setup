f8c82c0b0d036b1f9a54b8196d447d0718131f21
Justin Leo Ye <justinleoye@gmail.com>
2015-10-25 11:52:14 +0800

    basicly finished

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#########################################################################
+# File Name: setup.sh
+# Author: Justin Leo Ye
+# mail: justinleoye@gmail.com
+# Created Time: Sun Oct 25 11:42:32 2015
+#########################################################################
+#!/bin/bash
+
+# install pip
+wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
+
+python get-pip.py
+
+rm get-pip.py
+
+# install virtualenv
+pip install virtualenv
+
+
